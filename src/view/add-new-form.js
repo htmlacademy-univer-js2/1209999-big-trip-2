0f382fdc9d3ea0717fdd9886a3ece55a50543ca2
lev tarasov <56757711+levil664@users.noreply.github.com>
2023-03-13 16:22:15 +0500

    module1-task3

---
+++ src/view/add-new-form.js
@@ -0,0 +1,22 @@
+import { createElement } from '../render.js';
+
+const createNewAddFormTemplate = () => (
+  '<button class="trip-main__event-add-btn btn btn--big btn--yellow" type="button" disabled>New event</button>'
+);
+
+export default class FormView {
+  getTemplate() {
+    return createNewAddFormTemplate;
+  }
+
+  getElement() {
+    if(!this.element){
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
